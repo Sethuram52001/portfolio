10409fbf16005ce11a2502bc1fa51a4d6728b7f6
Sethuram <sethuram52001@gmail.com>
2021-05-23 23:04:13 +0530

    added show more show less functionality to certificates but bugs out due to some mistake in usage of useCallback hook

--- src/components/Certifications/Certfications.js
+++ src/components/Certifications/Certfications.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, {useState, useCallback} from 'react';
 import * as styles from "./Certifications.module.scss";
 import Certificate from '../Certificate/Certificate';
 import {certificates} from "./Certifications.data";
 
 const Certifications = () => {
 
+    const [limit, setLimit] = useState(6);
+    const [showStatus, setShowStatus] = useState("Show More");
+
+    const handleShow = useCallback(() => {
+        if(showStatus === "Show More") {
+            setLimit(certificates.length);
+            setShowStatus("Show Less");
+        }
+        else {
+            setLimit(6);
+            setShowStatus("Show more");
+        }
+    }, [showStatus, limit]);
+
     return ( 
         <div id="certifications" className={styles.certificationsSection}>
             <h1>Certifications</h1>
             <div className={styles.certificationsContainer}>
-                {certificates.map((certificate, index) => (
+                {certificates.slice(0, limit).map((certificate, index) => (
                     <Certificate name={certificate.name} organization={certificate.organization} verification={certificate.verification} />
                 ))}
             </div>
+            <button
+                onClick={handleShow}
+            >
+                {showStatus}
+            </button>
         </div>
      );
 }
