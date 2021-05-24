70a2eab6d15dfef6a586d71fefe8450c24ceca0a
Sethuram <sethuram52001@gmail.com>
2021-05-24 23:23:02 +0530

    added global styles, redesigned the about section - albeit needs final touch, added sociallinks components, added resume link

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,6 +2,8 @@ import React from 'react';
 import * as styles from "./About.module.scss";
 import Lottie from "react-lottie";
 import animationData from "../../assets/animations/student.json";
+import SocialLinks from '../SocialLinks/SocialLinks';
+import { Link } from 'gatsby';
 
 const About = () => {
 
@@ -12,21 +14,23 @@ const About = () => {
     }
 
     return ( 
-        <div id="about">
-            <h1 className={styles.title}>
-                Hi all I'm Sethuram
-                <span>
-                    <img src="https://twemoji.maxcdn.com/2/72x72/1f44b.png" alt="👋" />
-                </span>
-            </h1>
-            <div className={styles.aboutContainer}>
+        <div id="about" className={styles.aboutContainer}>
+            <div>
+                <h1 className={styles.title}>
+                    Hi all I'm Sethuram
+                    <span>
+                        <img src="https://twemoji.maxcdn.com/2/72x72/1f44b.png" alt="👋" />
+                    </span>
+                </h1>
                 <p className={styles.subtitle}>
                     I'm a passionate learner in the field of web-development, currently having fun building web-apps with ReactJS / Javascript and some othe cool libraries and frameworks.
                     I'm also intrigued by data structures and algorithms and occassionaly write blogs on them.
                 </p>
-                <div className={styles.lottieConatiner}>
-                    <Lottie options={defaultOptions} />
-                </div>
+                <SocialLinks />
+                <Link target="_blank" to="https://drive.google.com/drive/u/0/folders/1zKfGLvNqZTv6KGgjAUxJMPTOrhqUt1Lf">Resume</Link>
+            </div>
+            <div className={styles.lottieConatiner}>
+                <Lottie options={defaultOptions} />
             </div>
         </div>
      );
---
+++ src/components/SocialLinks/SocialLinks.js
@@ -0,0 +1,30 @@
+import { Link } from 'gatsby';
+import React from 'react';
+import GithubIcon from "../../assets/images/social-logos/github.svg";
+import LinkedInIcon from "../../assets/images/social-logos/linkedin.svg";
+import MediumIcon from "../../assets/images/social-logos/medium.svg";
+import * as styles from "./SocialLinks.module.scss";
+
+const SocialLinks = () => {
+    return ( 
+        <div>
+            <Link 
+                to="https://github.com/Sethuram52001"
+            >
+                <img className={styles.social_link} src={GithubIcon} alt="github" />
+            </Link>
+            <Link 
+                to="https://www.linkedin.com/in/sethuram-s-v-171719194/"
+            >
+                <img className={styles.social_link} src={LinkedInIcon} alt="linkedin" /> 
+            </Link>
+            <Link 
+                to="https://sethuram52001.medium.com/"
+            >
+                <img className={styles.social_link} src={MediumIcon} alt="medium" />
+            </Link>
+        </div>
+     );
+}
+ 
+export default SocialLinks;
