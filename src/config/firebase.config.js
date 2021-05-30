7701ec33299266e76725ac17f7fa416dd1ed170f
Sethuram <sethuram52001@gmail.com>
2021-05-30 22:56:53 +0530

    changed firebase keys to env file

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -1,13 +1,14 @@
 import firebase from 'firebase';
+require('dotenv').config();
 
 var firebaseConfig = {
-  apiKey: "AIzaSyD3JMPYOPlX-6zLpqK-URd4dAVPQMxyRAQ",
-  authDomain: "portfolio-52001.firebaseapp.com",
-  projectId: "portfolio-52001",
-  storageBucket: "portfolio-52001.appspot.com",
-  messagingSenderId: "888424064859",
-  appId: "1:888424064859:web:d4f2886d8631de722f9c7e",
-  measurementId: "G-BS3X0SYK01"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
