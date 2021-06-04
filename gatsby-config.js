df0a9c9feaef7bdf88d1f658ed3d7b7aad438f8d
Sethuram <sethuram52001@gmail.com>
2021-06-04 23:07:03 +0530

    took out the margin which were present in some components and added seo component and changed metadata in gatby file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Portfolio | Sethruam.S.V`,
+    description: `My portfolio site`,
+    author: `Sethuram.S.V`,
   },
   plugins: [
     `gatsby-plugin-sass`,
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,28 @@
 import React, { useContext } from 'react';
 import { GlobalStateContext } from '../../context/GlobalContextProvider';
+import {Helmet} from "react-helmet";
+import {useStaticQuery, graphql} from "gatsby";
 
 const Main = ({children}) => {
+
+    const data = useStaticQuery(graphql`
+        query SiteTitleQuery {
+            site {
+                siteMetadata {
+                    title
+                }
+            }
+        }
+    `)
 
     const isDark = useContext(GlobalStateContext);
 
     return ( 
-        <div className={isDark.theme === "dark" ? 'dark-theme' : 'light-theme' }>
+        <div className={isDark.theme === "dark" ? 'dark-theme' : 'light-theme'}>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>{data.title}</title>
+            </Helmet>
             {children}
         </div>
      );
